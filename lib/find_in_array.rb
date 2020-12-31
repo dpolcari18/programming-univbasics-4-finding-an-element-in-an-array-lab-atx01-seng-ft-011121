c026aa54df603bbd89ab17b4a8c6e247407566ae
David Polcari <dpolcari18@gmail.com>
2020-12-31 19:16:52 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -1,9 +1,5 @@
 def find_element_index(array, value_to_find)
- i=0
- while i<array.length do
-   if array[i]==value_to_find
-     return i
-   end
-   i+=1
- end
+  array.detect do |index|
+    value_to_find
+    return index
 end
