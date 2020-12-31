dd44906f493f581908ba8a2d4ee177162c52587c
David Polcari <dpolcari18@gmail.com>
2020-12-31 19:26:01 +0000

    Automatically backed up by Learn

--- lib/find_in_array.rb
+++ lib/find_in_array.rb
@@ -3,7 +3,7 @@ require 'pry'
 def find_element_index(array, value_to_find)
   array.detect do |index|
     value_to_find
-    binding.pry
+    #binding.pry
     return array.find_index(index)
   end
 end
